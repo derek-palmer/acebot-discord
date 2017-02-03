6b77b68b92a461c85bf2f6cfbef720766f6b1387
Derek Palmer <dp462@cornell.edu>
2017-02-03 08:42:38 -0500

    Removed console log statments, updated comments

--- app.js
+++ app.js
@@ -22,12 +22,11 @@ acebot.on('message', message => {
   if (message.content === '!bitcoin') {
     request('https://blockchain.info/ticker', function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body) //Show output
-        var result = JSON.parse(body); //Parse JSON Resultset
-        console.log(result); // Spit out parsed ResultSet
-        var USD = result.USD.last;
-        console.log(USD);
-        message.reply('the current Bitcoin market price is: $ ' + USD + ' USD');
+        console.log(body) //Show output in JSON
+        var result = JSON.parse(body); //Parse JSON Result
+        var USD = result.USD.last; //Set USD variable to the latest USD bitcoin price
+        console.log(USD); //Show price in console
+        message.reply('the current Bitcoin market price is: $ ' + USD + ' USD'); //Send price to user that requested price
       }
     })
   }
