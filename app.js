34acd3fdbda14b0e359e31aedd27ce8a07b95350
Derek Palmer <dp462@cornell.edu>
2017-02-02 19:11:21 -0500

    Added request to bitcoin api, parsed json output

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+var request = require('request');
 
 const Discord = require('discord.js');
 const bitbot = new Discord.Client();
@@ -21,5 +22,20 @@ bitbot.on('message', message => {
   }
 });
 
+bitbot.on('message', message => {
+  if(message.author.bot) return;
+  if (message.content === '!bitcoin-test') {
+    request('https://blockchain.info/ticker', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body) //Show output
+        var result = JSON.parse(body);
+        console.log(result);
+        var USD = result.USD.last;
+        console.log(result.USD.last);
+      }
+    })
+    message.reply.USD;
+  }
+});
 
 bitbot.login(process.env.LOGIN_TOKEN);
