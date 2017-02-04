a72944255e0833ac2a772207c9db6dce98eeafa9
Derek Palmer <derek-palmer@outlook.com>
2017-02-04 16:25:06 -0500

    Cleaned up app.js, added help command, updated git ignore

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
+/*jshint esversion: 6 */
 require('dotenv').config();
 var request = require('request');
 
 const Discord = require('discord.js');
 const acebot = new Discord.Client();
+const prefix = "!";
 
+//
 acebot.on('ready', () => {
-  console.log('I am ready!');
+    console.log('I am ready, gimmie some commands!!');
 });
 
-//Ping - Pong
 acebot.on('message', message => {
-  if (message.author.bot) return;
-  if (message.content === '!pong') {
-    message.reply('ping');
-  }
-});
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
 
-//Bitcoin
-acebot.on('message', message => {
-  if (message.author.bot) return;
-  if (message.content === '!bitcoin') {
-    request('https://blockchain.info/ticker', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body) //Show output in JSON
-        var result = JSON.parse(body); //Parse JSON Result
-        var USD = result.USD.last; //Set USD variable to the latest USD bitcoin price
-        console.log(USD); //Show price in console
-        message.reply('the current Bitcoin market price is: $ ' + USD + ' USD'); //Send price to user that requested price
-      }
-    })
-  }
+    //Help
+    if (message.content.startsWith(prefix + 'help')) {
+        message.reply("help? You don't need help.");
+    }
+    //Ping - Pong
+    if (message.content.startsWith(prefix + 'pong')) {
+        message.reply('ping');
+    }
+    //Bitcoin
+    if (message.content.startsWith(prefix + 'bitcoin')) {
+        request('https://blockchain.info/ticker', function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body); //Show output in JSON
+                var result = JSON.parse(body); //Parse JSON Result
+                var USD = result.USD.last; //Set USD variable to the latest USD bitcoin price
+                console.log(USD); //Show price in console
+                message.reply('the current Bitcoin market price is: $ ' + USD + ' USD'); //Send price to user that requested price
+            }
+        });
+    }
 });
 
 acebot.login(process.env.LOGIN_TOKEN);
