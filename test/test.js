05883390c316364ebe8b6bffb3e824a5f09d6ac1
Derek Palmer <derek-palmer@outlook.com>
2017-03-05 10:55:17 -0500

    declared variables ahead of if statment in tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
+/*jshint esversion: 6 */
 var assert = require('assert');
 
 describe('Commands', function() {
     describe('!add', function() {
         it('should return sum of numbers', function() {
             var command = "add 5 5 10 10";
+            var message;
             let args = command.split(" ").slice(1);
             if (command === "add 5 5 10 10") {
                 let numArray = args.map(n => parseInt(n));
                 let total = numArray.reduce((p, c) => p + c);
-                var message = total;
+                message = total;
             }
             assert.equal(message, 30);
         });
@@ -16,8 +18,9 @@ describe('Commands', function() {
     describe('!helpme', function() {
         it('should return help text as message', function() {
             var command = 'helpme';
+            var message;
             if (command === 'helpme') {
-                var message = "help? You don't need help.";
+                message = "help? You don't need help.";
             }
             assert.equal(message, "help? You don't need help.");
         });
@@ -25,8 +28,9 @@ describe('Commands', function() {
     describe('!pong', function() {
         it('should return ping as message', function() {
             var command = 'pong';
+            var message;
             if (command === 'pong') {
-                var message = "ping";
+                message = "ping";
             }
             assert.equal(message, "ping");
         });
@@ -34,9 +38,11 @@ describe('Commands', function() {
     describe('!bitcoin', function() {
         it('should return current bitcoin market price', function() {
             var command = 'bitcoin';
+            var message;
+            var USD;
             if (command === 'bitcoin') {
-                var USD = 1100;
-                var message = `the current Bitcoin market price is: $ ${USD} USD`;
+                USD = 1100;
+                message = `the current Bitcoin market price is: $ ${USD} USD`;
             }
             assert.equal(message, `the current Bitcoin market price is: $ ${USD} USD`);
         });
@@ -44,9 +50,11 @@ describe('Commands', function() {
     describe('!goat', function() {
         it('should return a goat gif', function() {
             var command = 'goat';
+            var message;
+            var goatURL;
             if (command === 'goat') {
-                var goatURL = 'https://media.giphy.com/media/5K3Vw3jUqwV56/giphy.gif';
-                var message = `${goatURL} :goat: | **Here is your random goat:**`;
+                goatURL = 'https://media.giphy.com/media/5K3Vw3jUqwV56/giphy.gif';
+                message = `${goatURL} :goat: | **Here is your random goat:**`;
             }
             assert.equal(message, `${goatURL} :goat: | **Here is your random goat:**`);
         });
@@ -54,9 +62,11 @@ describe('Commands', function() {
     describe('!kitten', function() {
         it('should return a kitten gif', function() {
             var command = 'kitten';
+            var message;
+            var kittenURL;
             if (command === 'kitten') {
-                var kittenURL = 'https://media.giphy.com/media/euVEp3YNqid5C/giphy.gif';
-                var message = `${kittenURL} :cat2: | **Here is your random kitten:**`;
+                kittenURL = 'https://media.giphy.com/media/euVEp3YNqid5C/giphy.gif';
+                message = `${kittenURL} :cat2: | **Here is your random kitten:**`;
             }
             assert.equal(message, `${kittenURL} :cat2: | **Here is your random kitten:**`);
         });
@@ -64,8 +74,9 @@ describe('Commands', function() {
     describe('!bringo', function() {
         it('should return steve brule bringo gif', function() {
             var command = 'bringo';
+            var message;
             if (command === 'bringo') {
-                var message = "https://media.giphy.com/media/xLsaBMK6Mg8DK/giphy.gif";
+                message = "https://media.giphy.com/media/xLsaBMK6Mg8DK/giphy.gif";
             }
             assert.equal(message, "https://media.giphy.com/media/xLsaBMK6Mg8DK/giphy.gif");
         });
@@ -73,8 +84,9 @@ describe('Commands', function() {
     describe('!triggered', function() {
         it('should return triggered gif', function() {
             var command = 'triggered';
+            var message;
             if (command === 'triggered') {
-                var message = "https://media.giphy.com/media/vk7VesvyZEwuI/giphy.gif";
+                message = "https://media.giphy.com/media/vk7VesvyZEwuI/giphy.gif";
             }
             assert.equal(message, "https://media.giphy.com/media/vk7VesvyZEwuI/giphy.gif");
         });
